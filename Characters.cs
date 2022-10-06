52f2b3122ba0da83a3b2090ed9d75e85ded96d5d
johanna <johanna@hemma.tk>
2022-10-06 15:21:37 +0200

    lagt till klasser

---
+++ Characters.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lektionsprojekt
+{
+    internal abstract class Characters 
+    {
+        string? Name { get; set; }
+        string? Description { get; set; }
+
+        protected int Hp { get; set; }
+        protected int Damage { get; set; }
+
+        protected abstract void Speak();
+
+       
+       
+    }
+}
---
+++ IMonster.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lektionsprojekt
+{
+    interface IMonster
+    {
+        string Type { get; }
+        string Description { get; }
+
+        bool IsHostile { get; }
+    }
+}
